7fa28594ca55c04dfc4998a42cb57ea07a665417
root <neerajkondalkar@yahoo.in>
2019-09-13 00:35:30 +0530

    inorived

--- script3.js
+++ script3.js
@@ -1,50 +1,61 @@
 function formValidation()
-{
-var name = document.registration.stdname;
-var add = document.registration.passidaddress;
-var eid = document.registration.emailid;
-var dob = document.registration.dob;
-{
-if(allLetter(name))
-{
-if(alphanumeric(add))
-{
-if(ValidateEmail(eid))
-{
-if(validdob(dob))
-{
+{	
+	validform=false;
+	var name = document.getElementById("stdname");
+	var add = document.getElementById("address");
+	var eid = document.getElementById("emailid");
+	var dob = document.getElementById("dob");
+	var branch = document.getElementByType("radio");
+	var i;
+	var radio_checked=false;
+	for (i=0; i<branch.length; i++){
+		if(branch[i].checked){
+			radio_checked=true;
+		}
+	}
+	var checkbox_checked;
+	var hobby = document.getElementByTag("checkbox");
+	for (i=0; i<hobby.length; i++){
+		if(hobby[i].checked){
+			checkbox_checked=true;
+		}
+	}
 }
-} 
-}
-} 
-}
-}
-}
-}
-return false;
-}
-function alphanumeric(add)
+
+if(!(name.length==add.length==eid.length==dob.length=="") && checkbox_checked && radio_checked ){
+
+			if(allLetter(name)){
+				if(validEmail(eid))
+				{
+					if(validDob(dob))
+					{	
+						validform=true;		
+					}
+				}
+				else{
+					alert("You have entered an invalid email address!");
+					eid.focus();
+				} 
+			}
+			else{
+				alert("You have entered an invalid Name");
+				name.focus();	
+			}
+}	
+
+function validEmail(eid)
 {
-var address_len = address.value.length;
-if (address_len == 0)
-{
-alert("this field should not be empty);
-address.focus();
-return false;
+	var mailformat = "/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/";
+	if(eid.match(mailformat))
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
-return true;
-}
-function ValidateEmail(eid)
-{
-var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-if(uemail.value.match(mailformat))
-{
-return true;
-}
-else
-{
-alert("You have entered an invalid email address!");
-uemail.focus();
-return false;
-}
+
+if(validform){
+	document.write("Welcome".name);
 }
